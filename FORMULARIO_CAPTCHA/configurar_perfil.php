4542c23ba587c209e11787fb4915949181e35ca7
Javier Puerto Romero <130344358+JavierPuerto@users.noreply.github.com>
2023-10-20 09:02:29 +0200

    Update configurar_perfil.php

--- FORMULARIO_CAPTCHA/configurar_perfil.php
+++ FORMULARIO_CAPTCHA/configurar_perfil.php
@@ -14,9 +14,11 @@
     }
 
     include 'php/conexion_be.php';
+    include 'php/conexionpoblacion.php';
     $correo=$_SESSION['correo'];
     $query="SELECT * FROM usuarios WHERE Usuario_email='$correo'";
     $ejecutar=mysqli_query($conexion,$query);
+
 
 ?>
 
@@ -28,6 +30,23 @@
     <title>Verificar Datos</title>
     <link rel="stylesheet" href="assets/css/estiloConfigurar_perfil.css">
 </head>
+<style>
+        body {
+            animation: changeBackground 10s infinite alternate;
+        }
+
+        @keyframes changeBackground {
+            0% {
+                background-color: #5aac2d; 
+            }
+            50%{
+                background-color :#33ff57;
+            }
+            100% {
+                background-color: #33ff9c; 
+            }
+        }
+    </style>
 <body>
     <main>
         <div class="contenedor__todo">
@@ -52,16 +71,54 @@
                 <?php
                 echo "<br>";
                 ?>
-                Poblacion:
-                <input type="text" name="poblacion" value="<?php echo $reg['Usuario_poblacion'] ?>" >
-                <?php
-                echo "<br>";
-                ?>
                 Provincia:
-                <input type="text" name="provincia" value="<?php echo $reg['Usuario_provincia'] ?>" >
-                <?php
-                echo "<br>";
-                ?>
+<select name="provincia" id="provinciaSelect">
+    <?php
+    $query_provincias = "SELECT * FROM provincias"; 
+    $result_provincias = mysqli_query($conexionPoblacion, $query_provincias);
+
+    while($row_provincia = mysqli_fetch_assoc($result_provincias)){
+        $selected = ($row_provincia['Provincia'] == $reg['Usuario_provincia']) ? "selected" : "";
+        echo "<option value='".$row_provincia['Provincia']."' ".$selected.">".$row_provincia['Provincia']."</option>";
+    }
+    ?>
+</select>
+<br>
+
+
+Municipio:
+<select name="municipio" id="municipioSelect">
+    <?php
+    $provincia_elegida = $reg['Usuario_provincia'];
+    $query_municipios = "SELECT * FROM municipios WHERE idProvincia IN (SELECT idProvincia FROM provincias WHERE Provincia='$provincia_elegida')";
+    $result_municipios = mysqli_query($conexionPoblacion, $query_municipios);
+
+    while($row_municipio = mysqli_fetch_assoc($result_municipios)){
+        $selected = ($row_municipio['Municipio'] == $reg['Usuario_poblacion']) ? "selected" : "";
+        echo "<option value='".$row_municipio['Municipio']."' ".$selected.">".$row_municipio['Municipio']."</option>";
+    }
+    ?>
+</select>
+<script>
+document.getElementById('provinciaSelect').addEventListener('change', function() {
+    var provinciaSeleccionada = this.value;
+    var municipioSelect = document.getElementById('municipioSelect');
+
+    // Realiza una petición AJAX para obtener los municipios
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'obtener_municipios.php', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            municipioSelect.innerHTML = xhr.responseText;
+        }
+    };
+
+    xhr.send('provincia=' + provinciaSeleccionada);
+});
+</script>
+<br>
                 Dni:
                 <input type="text" name="nif" value="<?php echo $reg['Usuario_nif'] ?>" >
                 <?php

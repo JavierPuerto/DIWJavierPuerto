99d6b196ad5c089184d9da567d63d0d7e29c17c6
Javier Puerto Romero <130344358+JavierPuerto@users.noreply.github.com>
2023-10-23 09:09:43 +0200

    Meterle geolocalizacion a bienvenido.php
    He añadido la api de google

--- FORMULARIO_CAPTCHA/bienvenido.php
+++ FORMULARIO_CAPTCHA/bienvenido.php
@@ -43,9 +43,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/estiloBienvenido.css">
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjFQbNK4VVgSYysAZoMP598EPMRK3G5tY&libraries=places"></script>
     <title>Formulario 2DAW</title>
-</head>
-<style>
+    <style>
         body {
             animation: changeBackground 10s infinite alternate;
         }
@@ -61,21 +61,62 @@
                 background-color: #33ff9c; 
             }
         }
+
+        .map-container {
+            width: 300px; /* Ajusta el ancho del contenedor del mapa según tu preferencia */
+            height: 200px; /* Ajusta la altura del contenedor del mapa según tu preferencia */
+            margin-top: 20px;
+            border: 2px solid #ccc; /* Añade un borde alrededor del mapa */
+            border-radius: 5px;
+            margin-left: 866px;
+        }
     </style>
+</head>
 <body>
     <h1>Bienvenido</h1>
     <?php
     if($usuario == "admin"){
         ?>
-<a class ="menu_admin-button" href="php/listado.php">Menu de Administrador</a>
-<?php
+        <a class ="menu_admin-button" href="php/listado.php">Menu de Administrador</a>
+    <?php
     }
     ?>
-    
+   
     <a class="cerrar_sesion-button" href="php/cerrar_sesion.php">Cerrar Sesión</a>
     <a class="configurar_perfil-button" href="configurar_perfil.php">Configurar Perfil</a>
     <a class="mostrar_datos-button" href="php/mostrar_datos.php">Mostrar Datos</a>
     <img class="foto" src="data:image/jpeg;base64,<?php echo $imagen_usuario; ?>" alt="Imagen de usuario">
 
+    <!-- Contenedor para el mapa -->
+    <div class="map-container">
+        <div id="googleMap" style="width: 100%; height: 100%;"></div>
+    </div>
+
+    <script>
+        function initMap() {
+            if ("geolocation" in navigator) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var userLocation = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+
+                    var mapOptions = {
+                        center: userLocation,
+                        zoom: 15
+                    };
+                    var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+
+                    var marker = new google.maps.Marker({
+                        position: userLocation,
+                        map: map,
+                        title: 'Tu ubicación'
+                    });
+                });
+            }
+        }
+    </script>
+
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjFQbNK4VVgSYysAZoMP598EPMRK3G5tY&callback=initMap"></script>
 </body>
 </html>
